685234cef9439066488bebaa6d60ca4241449618
jamesritzman <jamesritzman@yahoo.com>
2021-10-08 20:23:15 -0700

    basic styling for mobile breakpoint

--- src/components/PolicyCard/PolicyCard.js
+++ src/components/PolicyCard/PolicyCard.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import "./PolicyCard.scss";
 
 const PolicyCard = (props) => {
     const {
@@ -16,11 +17,40 @@ const PolicyCard = (props) => {
         partner
     } = props.data;
 
+    const formatDate = (dateString) => {
+        const dateElements = dateString.split("-");
+        const month = ["JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"][dateElements[1] - 1];
+        return `${dateElements[2]}-${month}-${dateElements[0]}`;
+    };
+
+    const coverageDatesSecondaryInfo = () => {
+        return coverage_end_date ?
+            (
+                <div className="coverage-dates-secondary">
+                    <p>Coverage dates</p><p className="status-text-active">Active &bull;</p>
+                </div>
+            )
+            : (
+                <div className="coverage-dates-secondary">
+                    <p>Date shipped</p><p className="status-text-expired">Expired &bull;</p>
+                </div>
+            )
+    };
+
     return(
-        <div>
-            <h3>{title}</h3>
-            <p>{id} | {description}</p>
-            <p>{coverage_start_date} {coverage_end_date ? ` to ${coverage_end_date}` : ""}</p>
+        <div className="policy-card">
+            <div className="policy-card-top">
+                <h3>{title}</h3>
+                <p>{id} | {description}</p>
+            </div>
+            <div className="rule"></div>
+            <div className="policy-card-bottom">
+                <div className="policy-card-coverage-date-info">
+                    <p className="coverage-dates">{formatDate(coverage_start_date)} {coverage_end_date ? ` to ${formatDate(coverage_end_date)}` : ""}</p>
+                    { coverageDatesSecondaryInfo()}                    
+                </div>
+                <img src={partner.logo} className="logo" alt="company logo" />
+            </div>
         </div>
     )
 };
